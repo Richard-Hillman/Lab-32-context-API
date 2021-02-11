eade54e58d16df042b2a35923207a2c948f59a73
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-02-10 21:15:50 -0800

    successful transfer of files to change over to context api minor name changes to file tree,

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,19 +1,20 @@
+/* eslint-disable max-len */
 import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom';
-import CharactersList from '../../containers/CharacterList';
-import CharacterDetails from '../../containers/CharacterDetails';
+import CharacterListContainer from '../../containers/CharacterListContainer';
+import IndividualCharacterContainer from '../../containers/IndividualCharacterContainer';
 
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={CharactersList} />
-        <Route path="/characters/:name" component={CharacterDetails} />
+        <Route exact path="/" component={CharacterListContainer} />
+        <Route path="/characters/:name" component={IndividualCharacterContainer} />
       </Switch>
     </Router>
   );
---
+++ src/containers/CharacterListContainer.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+import React from 'react';
+import CharacterListPre from '../components/Presentational/CharacterListPre';
+import LoadingSpinner from '../components/loading/Loading';
+import { useCharacterList } from '../hooks/useCharacterList';
+
+const CharacterList = () => {
+  const { loading, characters } = useCharacterList();
+
+  if(loading) return <LoadingSpinner />;
+  return <CharacterListPre characters={characters} />;
+};
+
+export default CharacterList;
